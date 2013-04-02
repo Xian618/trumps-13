fd7b8694bc3fda5cb4f450c7418489001d3b929d
Alex Mackay <alex.mackay618@gmail.com>
2013-04-02 22:06:07 +0100

    Change routing to mostly automatic, add stub methods to game controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,13 @@
 class GameController < ApplicationController
   def index
+  	render :text => "index method"
+  end
+
+  def show
+  	render :text => "show method"
+  end
+
+  def populate
+  	render :text => "populate method"
   end
 end
