658e6cbf50ffa228d0ebd6445d93b38121fc24e0
Alex Mackay <alex.mackay618@gmail.com>
2013-04-02 23:27:11 +0100

    Add XLS download and parsing capability

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,24 +1,47 @@
 class GameController < ApplicationController
   require 'json'
   require 'net/http'
+  require 'spreadsheet'
 
   def index
-  	render :text => "index method"
+    render :text => "index method"
   end
 
   def show
-  	render :text => "show method"
+    render :text => "show method"
   end
 
   def populate
-  	meta_url = "http://data.gov.uk/api/2/rest/package/ni-112-under-18-conception-rate"
+    url = getdataseturl()
+    file = savedataset(url)
+    readdataset(file)
+  end
+
+private
+  def getdataseturl
+    meta_url = "http://data.gov.uk/api/2/rest/package/ni-112-under-18-conception-rate"
     meta_uri = URI(meta_url)
     meta_json = Net::HTTP.get(meta_uri)
     meta_hash = JSON.parse(meta_json)
-    
-    dataset_url = meta_hash["resources"][0]["url"]
-    dataset_uri = URI(dataset_url)
+    return meta_hash["resources"][0]["url"]
+  end
+
+  def savedataset(url)
+    temppath = "temp.xls"
+    dataset_uri = URI(url)
     dataset = Net::HTTP.get(dataset_uri)
-    render :text => dataset
+    
+    File.open(temppath, "wb") do |f|
+      f.write(dataset)
+    end
+
+    return temppath
+  end
+
+  def readdataset(file)
+    dataset = Spreadsheet.open(file)
+    
+    # use dataset here 
   end
 end
+
