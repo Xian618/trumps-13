e17e015f0be901cae9f47df5f3431891c0c0e8f8
Alex Mackay <alex.mackay618@gmail.com>
2013-04-02 22:34:01 +0100

    Add initial dataset retreival

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,7 @@
 class GameController < ApplicationController
+  require 'json'
+  require 'net/http'
+
   def index
   	render :text => "index method"
   end
@@ -8,6 +11,14 @@ class GameController < ApplicationController
   end
 
   def populate
-  	render :text => "populate method"
+  	meta_url = "http://data.gov.uk/api/2/rest/package/ni-112-under-18-conception-rate"
+    meta_uri = URI(meta_url)
+    meta_json = Net::HTTP.get(meta_uri)
+    meta_hash = JSON.parse(meta_json)
+    
+    dataset_url = meta_hash["resources"][0]["url"]
+    dataset_uri = URI(dataset_url)
+    dataset = Net::HTTP.get(dataset_uri)
+    render :text => dataset
   end
 end
