e04ef8273e8545daf2cb22b160059886cbaeda68
Alex Mackay <alex.mackay618@gmail.com>
2013-04-06 14:58:11 +0100

    Implement part of TurnUpdate

--- app/models/turnupdate.rb
+++ app/models/turnupdate.rb
@@ -1,3 +1,12 @@
 class TurnUpdate
-  attr_accessible :stat_picked, :you_win_hand, :you_win_game
+
+  include AttributeHashHelper
+
+  attr_accessor :stat_picked, :you_win_hand, :you_win_game
+
+  def send_to_player(game_id, player_id)
+ 	queue_name = 'presence-'+game_id.to_s+'-'+player_id.to_s
+ 	Pusher.trigger(queue_name, 'turnupdate', to_hash())
+  end
+
 end
