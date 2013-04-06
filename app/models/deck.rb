3ba02de95a12ca154639b9efb13d1fc55592430d
Alex Mackay <alex.mackay618@gmail.com>
2013-04-06 12:20:06 +0100

    Add deck shuffle method

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,16 @@
 class Deck < ActiveRecord::Base
   belongs_to :player
   has_many :cards
+
+  def self.shuffle_into_decks(stat_type, deck1, deck2)
+    cards = Card.find_all_by_subtitle(stat_type).shuffle
+    if(cards.size.odd?)
+    	cards = cards.first(cards.size - 1)
+    end
+
+    cards.each_slice(2) do |a, b|
+    	deck1.cards << a
+    	deck2.cards << b
+    end
+  end
 end
